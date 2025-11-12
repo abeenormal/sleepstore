13f11627a5f6eea79ae888d311fb822e73e251a9
hickmb34@gmail.com <hickmb34@gmail.com>
2025-11-12 20:56:54 +0000

    Edited MyPurchases

--- client_code/MyPurchases/__init__.py
+++ client_code/MyPurchases/__init__.py
@@ -19,11 +19,13 @@ from ..Product import Product
 
 class MyPurchases(MyPurchasesTemplate):
   def __init__(self, **properties):
-  
+   self.load_products
        
     # Set Form properties and Data Bindings.
    self.init_components(**properties)
 
+
+  
   def load_products(self):
     products = anvil.server.call("get_purchased_items")
 
